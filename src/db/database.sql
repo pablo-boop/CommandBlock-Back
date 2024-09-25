7411db610936e2f37eccc3adec4d90863deb7b41
Caio Cicca <caio.ciccarelli@aluno.senai.br>
2024-09-25 17:13:05 -0300

    feat: create vacancy controller

--- src/db/database.sql
+++ src/db/database.sql
@@ -26,11 +26,12 @@ CREATE TABLE vacancies (
     id SERIAL PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     description TEXT NOT NULL,
-    creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creation_time DATE,
     expiration_time DATE NOT NULL,
-    status VARCHAR(100) NOT NULL,
+    status VARCHAR(100) NOT NULL DEFAULT 'sem candidatos',
     type VARCHAR(150) NOT NULL
 );
+
 CREATE TABLE candidacies (
     id SERIAL PRIMARY KEY,
     id_student INT NOT NULL,
@@ -42,9 +43,41 @@ CREATE TABLE candidacies (
     done BOOLEAN,
     hired BOOLEAN,
     description TEXT,
-    cretion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modification_data DATE,
     FOREIGN KEY (id_student) REFERENCES users(id),
     FOREIGN KEY (id_vacancy) REFERENCES vacancies(id),
     FOREIGN KEY (id_company) REFERENCES companies(id)
 );
+
+CREATE OR REPLACE FUNCTION update_vacancy_status()
+RETURNS TRIGGER AS $$
+DECLARE
+    candidacy_record RECORD;
+BEGIN
+    SELECT * INTO candidacy_record FROM candidacies WHERE id_vacancy = NEW.id_vacancy;
+
+    IF candidacy_record.iniciated THEN
+        IF candidacy_record.hired THEN
+            UPDATE vacancies SET status = 'contratado' WHERE id = NEW.id_vacancy;
+        ELSIF candidacy_record.done THEN
+            UPDATE vacancies SET status = 'finalizada' WHERE id = NEW.id_vacancy;
+        ELSIF candidacy_record.documentsManagement THEN
+            UPDATE vacancies SET status = 'gerenciamento de documentos' WHERE id = NEW.id_vacancy;
+        ELSIF candidacy_record.curriculumAvaliation THEN
+            UPDATE vacancies SET status = 'avaliação de curriculo' WHERE id = NEW.id_vacancy;
+        ELSE
+            UPDATE vacancies SET status = 'iniciada' WHERE id = NEW.id_vacancy;
+        END IF;
+    ELSE
+        UPDATE vacancies SET status = 'sem candidatos' WHERE id = NEW.id_vacancy;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER candidacy_status_update
+AFTER INSERT OR UPDATE ON candidacies
+FOR EACH ROW
+EXECUTE FUNCTION update_vacancy_status();
--- src/routes/vacancy.routes.js
+++ src/routes/vacancy.routes.js
@@ -7,6 +7,5 @@ router.get('/vacancies/:id', vacancyController.getVacancyById);
 router.post('/vacancies', vacancyController.createVacancy);
 router.put('/vacancies/:id', vacancyController.editVacancy);
 router.delete('/vacancies/:id', vacancyController.deleteVacancy);
-router.post('/vacancies/:id/students', vacancyController.addStudentVacancy)
 
 module.exports = router;
